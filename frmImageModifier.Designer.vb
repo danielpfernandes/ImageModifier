76ce826840b905039d7a0d24a27e543e10309e3a
danielpfernandes <daniel.paivafernandes@gmail.com>
2016-04-20 09:16:34 -0400

    Initial Commit

---
+++ frmImageModifier.Designer.vb
@@ -0,0 +1,162 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmImageModifier
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnOpen = New System.Windows.Forms.Button()
+        Me.btnConvert = New System.Windows.Forms.Button()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.cboFilter = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.picConverted = New System.Windows.Forms.PictureBox()
+        Me.picOriginal = New System.Windows.Forms.PictureBox()
+        CType(Me.picConverted, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picOriginal, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnOpen
+        '
+        Me.btnOpen.BackColor = System.Drawing.Color.Maroon
+        Me.btnOpen.FlatAppearance.BorderSize = 0
+        Me.btnOpen.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnOpen.ForeColor = System.Drawing.Color.White
+        Me.btnOpen.Location = New System.Drawing.Point(32, 38)
+        Me.btnOpen.Name = "btnOpen"
+        Me.btnOpen.Size = New System.Drawing.Size(124, 32)
+        Me.btnOpen.TabIndex = 0
+        Me.btnOpen.Text = " Open Image"
+        Me.btnOpen.UseVisualStyleBackColor = False
+        '
+        'btnConvert
+        '
+        Me.btnConvert.BackColor = System.Drawing.Color.DarkSeaGreen
+        Me.btnConvert.FlatAppearance.BorderSize = 0
+        Me.btnConvert.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnConvert.ForeColor = System.Drawing.Color.White
+        Me.btnConvert.Location = New System.Drawing.Point(440, 38)
+        Me.btnConvert.Name = "btnConvert"
+        Me.btnConvert.Size = New System.Drawing.Size(75, 32)
+        Me.btnConvert.TabIndex = 1
+        Me.btnConvert.Text = "Convert"
+        Me.btnConvert.UseVisualStyleBackColor = False
+        '
+        'btnSave
+        '
+        Me.btnSave.BackColor = System.Drawing.Color.Green
+        Me.btnSave.FlatAppearance.BorderSize = 0
+        Me.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnSave.ForeColor = System.Drawing.Color.White
+        Me.btnSave.Location = New System.Drawing.Point(550, 38)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(75, 32)
+        Me.btnSave.TabIndex = 2
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = False
+        '
+        'cboFilter
+        '
+        Me.cboFilter.BackColor = System.Drawing.Color.Indigo
+        Me.cboFilter.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.cboFilter.ForeColor = System.Drawing.Color.White
+        Me.cboFilter.FormattingEnabled = True
+        Me.cboFilter.Items.AddRange(New Object() {"   Monochrome", "   Gray Scale"})
+        Me.cboFilter.Location = New System.Drawing.Point(187, 37)
+        Me.cboFilter.Name = "cboFilter"
+        Me.cboFilter.Size = New System.Drawing.Size(172, 24)
+        Me.cboFilter.TabIndex = 3
+        Me.cboFilter.Text = "   Please select a filter"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.ForeColor = System.Drawing.Color.LightSteelBlue
+        Me.Label1.Location = New System.Drawing.Point(437, 99)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 17)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Preview"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.ForeColor = System.Drawing.Color.LightSteelBlue
+        Me.Label2.Location = New System.Drawing.Point(29, 99)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(57, 17)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Original"
+        '
+        'picConverted
+        '
+        Me.picConverted.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.picConverted.Location = New System.Drawing.Point(440, 128)
+        Me.picConverted.Name = "picConverted"
+        Me.picConverted.Size = New System.Drawing.Size(387, 291)
+        Me.picConverted.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picConverted.TabIndex = 6
+        Me.picConverted.TabStop = False
+        '
+        'picOriginal
+        '
+        Me.picOriginal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.picOriginal.Location = New System.Drawing.Point(32, 128)
+        Me.picOriginal.Name = "picOriginal"
+        Me.picOriginal.Size = New System.Drawing.Size(387, 291)
+        Me.picOriginal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picOriginal.TabIndex = 7
+        Me.picOriginal.TabStop = False
+        '
+        'frmImageModifier
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(859, 447)
+        Me.Controls.Add(Me.picOriginal)
+        Me.Controls.Add(Me.picConverted)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cboFilter)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.btnConvert)
+        Me.Controls.Add(Me.btnOpen)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "frmImageModifier"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Image Modifier"
+        CType(Me.picConverted, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picOriginal, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnOpen As Button
+    Friend WithEvents btnConvert As Button
+    Friend WithEvents btnSave As Button
+    Friend WithEvents cboFilter As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents picConverted As PictureBox
+    Friend WithEvents picOriginal As PictureBox
+End Class
---
+++ frmImageModifier.vb
@@ -0,0 +1,74 @@
+﻿Public Class frmImageModifier
+
+    Dim myImage As Bitmap
+
+    Private Sub btnOpen_Click(sender As Object, e As EventArgs) Handles btnOpen.Click
+        Dim open As New OpenFileDialog
+        open.Title = "Image Location"
+        open.Filter = "JPEG Image |*.jpg|All filed (*.*)|*.*"
+        If open.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            myImage = New Bitmap(open.FileName, True)
+            picOriginal.BorderStyle = BorderStyle.None
+            picOriginal.Image = myImage
+            picConverted.Image = Nothing
+        End If
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        If picConverted.Image IsNot Nothing Then
+            Dim save As New SaveFileDialog
+            save.Title = "Save Folder"
+            save.Filter = "JPEG Image |*.jpg|All filed (*.*)|*.*"
+            If save.ShowDialog() = Windows.Forms.DialogResult.OK Then
+                picConverted.Image.Save(save.FileName)
+                MessageBox.Show("The converted image has benn successfully saved!")
+            End If
+        Else
+            MessageBox.Show("There is no image to be saved.", "Error")
+        End If
+    End Sub
+
+    Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+        If picOriginal.Image IsNot Nothing Then
+            myImage = New Bitmap(picOriginal.Image)
+            Dim r, g, b, average As Integer
+            Dim newColor As Color
+
+            If cboFilter.SelectedItem IsNot Nothing Then
+                For y = 0 To myImage.Height - 1
+                    For x = 0 To myImage.Width - 1
+                        Dim pixelColor As Color = myImage.GetPixel(x, y)
+                        r = CInt(pixelColor.R)
+                        g = CInt(pixelColor.G)
+                        b = CInt(pixelColor.B)
+
+                        Select Case cboFilter.SelectedItem.ToString
+                            Case "   Monochrome"
+                                average = CInt((r + g + b) / 3)
+                                If (average > 128) Then
+                                    r = 255
+                                    g = 255
+                                    b = 255
+                                Else
+                                    r = 0
+                                    g = 0
+                                    b = 0
+                                End If
+                                newColor = Color.FromArgb(r, g, b)
+                            Case "   Gray Scale"
+                                average = CInt((r + g + b) / 3)
+                                newColor = Color.FromArgb(average, average, average)
+                        End Select
+                        myImage.SetPixel(x, y, newColor)
+                    Next
+                Next
+                picConverted.Image = myImage
+                picConverted.BorderStyle = BorderStyle.None
+            Else
+                MessageBox.Show("Please select a filter to work with.", "Error")
+            End If
+        Else
+            MessageBox.Show("Please select a picture to convert.", "Error")
+        End If
+    End Sub
+End Class
